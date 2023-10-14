a198bb3d34748aeda998859bb8cfa5416d2e1695
jimmyDef <jimmydef@outlook.fr>
2023-10-14 12:50:34 +0200

    fix(page media): correction pour calcul de like total dynamique

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -3,15 +3,21 @@ import { getData } from "../utils/modules.js";
 const url = new URL(window.location.href);
 const id = url.searchParams.get("id");
 
+//-----------------------------------------------------
+// Fonction création des infos photographe
+//-----------------------------------------------------
+
 const displayPhotographer = (photographer) => {
   photographerTemplate(photographer).userInfo();
 };
 
+//-----------------------------------------------------
+// Fonction création  des cards
+//-----------------------------------------------------
+
 const displayMedia = async (media) => {
   const mediaSection = document.getElementById("media-section");
   const userMedia = media.filter((media) => media.photographerId == id);
-  const totalLikes = userMedia.reduce((a, b) => b.likes + a, 0);
-  document.getElementById("total").textContent = totalLikes;
 
   userMedia.forEach((data) => {
     const mediaModel = mediaTemplate(data);
@@ -21,6 +27,42 @@ const displayMedia = async (media) => {
   });
 };
 
+//-----------------------------------------------------
+// Fonction gestion des Likes
+//-----------------------------------------------------
+const displaylikes = () => {
+  const mediaLike = document.querySelectorAll(".media__likes-number");
+
+  mediaLike.forEach((elt) => {
+    const likesBox = elt.closest(".media__likes");
+    let isLiked = false;
+    likesBox.addEventListener("click", function () {
+      if (isLiked) {
+        elt.textContent = parseInt(elt.textContent) - 1;
+        isLiked = !isLiked;
+      } else {
+        elt.textContent = parseInt(elt.textContent) + 1;
+        isLiked = !isLiked;
+      }
+      totalCounted();
+    });
+  });
+
+  const totalCounted = () => {
+    const mediaLikeArray = Array.from(mediaLike);
+
+    const likesTotal = mediaLikeArray.reduce(
+      (a, b) => parseInt(b.textContent) + a,
+      0
+    );
+    document.getElementById("total").textContent = likesTotal;
+  };
+  totalCounted();
+};
+
+//-----------------------------------------------------
+// Fonction initialisation de la page
+//-----------------------------------------------------
 const init = async () => {
   const { photographers, media } = await getData(
     "./../../data/photographers.json"
@@ -29,6 +71,7 @@ const init = async () => {
 
   displayPhotographer(photographer);
   displayMedia(media);
+  displaylikes();
 };
 
 init();
